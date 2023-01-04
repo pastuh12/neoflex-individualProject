f6d97843f62c749ab0a9a948ac77fd6f73bc35f1
Ilya Tarasov <ilyatarasov75@gmail.com>
2023-01-04 14:11:39 +0300

    add db connection

--- project/src/index.ts
+++ project/src/index.ts
@@ -1,29 +1,38 @@
+
 import * as dotenv from "dotenv";
 import * as bodyParser from "body-parser";
 import express from "express";
-import * as logger from "morgan";
-import * as Pg from "pg";
+import logger from "morgan";
 
-const postgres = new Pg({
-    user: "admin",
-    host: "localhost",
-    database: "creditСonveyor",
-    paswword: "admin",
-    port: 5432,
-});
+import sequelize from "./db";
+
 const app = express();
 
 dotenv.config();
 
-const port = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080
+const APP_MODE = process.env.APP_MODE || "dev"
 
 app.use(bodyParser.json());
+app.use(logger(APP_MODE))
 app.use(
     bodyParser.urlencoded({
         extended: true,
     })
 )
-app.get("/", (request, response) => {
-    response.send("Hello world!");
-});
-app.listen(port, () => console.log(`Running on port ${port}`));
+
+const start = async function(){
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.get("/", (request, response) => {
+            response.send("Hello world!");
+        });
+        app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+        
+    } catch (error) {
+        
+    }
+}
+
+start()
