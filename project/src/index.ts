f7481698ee314fd72d2b3c3b302b3362a9b1cda5
Tarasov ILya <ilyatarasov75@gmail.com>
2023-01-04 14:31:33 +0300

    create connect to database

--- project/src/index.ts
+++ project/src/index.ts
@@ -1,10 +1,28 @@
 import * as dotenv from "dotenv";
+import * as bodyParser from "body-parser";
 import express from "express";
+import * as logger from "morgan";
+import * as Pg from "pg";
+
+const postgres = new Pg({
+    user: "admin",
+    host: "localhost",
+    database: "creditСonveyor",
+    paswword: "admin",
+    port: 5432,
+});
+const app = express();
 
 dotenv.config();
 
-const app = express();
 const port = process.env.PORT || 8080
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
 app.get("/", (request, response) => {
     response.send("Hello world!");
 });
